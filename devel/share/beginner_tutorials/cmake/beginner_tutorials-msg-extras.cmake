4ba3b8bec2f9c48232610e819259853362a8a37d
Youbiao He <hybtalented@163.com>
2022-01-05 19:56:59 +0800

    编写和编译 ros 服务定义文件

--- devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
+++ devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(beginner_tutorials_MESSAGE_FILES "/home/hybtalented/rpi-tools/ros_study/catkin_ws/src/beginner_tutorials/msg/Num.msg")
-set(beginner_tutorials_SERVICE_FILES "")
+set(beginner_tutorials_SERVICE_FILES "/home/hybtalented/rpi-tools/ros_study/catkin_ws/src/beginner_tutorials/srv/AddTwoInts.srv")
